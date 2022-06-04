31e51a70ffa8628a6c9513bf986ddc5ce6707507
Reaprrz <57453003+Reaprrz@users.noreply.github.com>
2022-06-04 10:07:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,26 @@ import os
 #import dnspython
 import server
 
-
-bot = commands.Bot(command_prefix="!")
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix='?', intents=intents)
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+    print(f"Logged in as {bot.user}")
+    activity = discord.Game(name="/arcticsmp in status for Warrior Role", type=3)
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
+    checkstatus.start()
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+async def hello(ctx):
+    await ctx.send("Hello, I am a robot")
 
 @tasks.loop(seconds=5)
 async def checkstatus():
 
     #await bot.wait_until_ready() # waits for the bot to be ready until it starts
+
 
     guild = bot.get_guild(970480476747857940)
 
@@ -33,18 +37,9 @@ async def checkstatus():
 
     for member in guild.members: # for every member in the server
 
-            if(role not in member.roles): #check if member doesnt have the role 
-                print()
-            else:
-                await member.remove_roles(role) #removes the role if the member doesnt have the status
-
-                embed=discord.Embed(title="Role Removed", description="Status", color=0xff1a1a)
-                embed.add_field(name=member, value="Went Offline", inline=False)
-                embed.set_footer(text = f'{current_Time} , {current_Date}')
-                await channel.send(embed=embed)
-
+                
         for status in member.activities: # for every status that member has.
-
+        
             if isinstance(status, discord.CustomActivity):
 
                 if('arcticsmp' in str(status)): #if test is in the members status
@@ -54,22 +49,23 @@ async def checkstatus():
                     else:
                         await member.add_roles(role) #gives the pic perms roles to the members with the status
 
-                        embed=discord.Embed(title="Role Given", description="Status", color=0x00ff00)
-                        embed.add_field(name=member, value="/arcticsmp Was In Status", inline=False)
-                        embed.set_footer(text = f'{current_Time} , {current_Date}')
+                        embed=discord.Embed(title="Role Given", description="Warrior Status", color=0x00ff00)
+                        embed.add_field(name=member, value="Thank you for supporting us by putting our vanity in your status! ✔️", inline=False)
+                        
                         await channel.send(embed=embed)
-
+                
                 else:
 
                     if(role in member.roles): #check if members have the role 
                         await member.remove_roles(role) #removes the role if the member doesnt have the status
-                        embed=discord.Embed(title="Role Removed", description="Status", color=0xff1a1a)
-                        embed.add_field(name=member, value="/= arcticsmp Not In Status", inline=False)
-                        embed.set_footer(text = f'{current_Time} , {current_Date}')
-                        await channel.send(embed=embed)
-
+                        
+                    
                     else:
                         print()
-    
+                    
+                
+                    
+                        
+ #starts the loop
 server.server()
 client.run(TOKEN)
